5791e3419340778b4c6e834c9d813a64d6a38c2a
c0nhue <maxsnp8@gmail.com>
2025-09-02 14:58:29 +0400

    fixed input shape

--- src/model.py
+++ src/model.py
@@ -1,9 +1,11 @@
+from tensorflow import keras
 from tensorflow.keras import layers, models
 
-def build_model(input_shape=(28, 28, 1), num_classes=10):
+def build_model(num_classes=10):
     """Define CNN model for MNIST classification."""
     model = models.Sequential([
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=input_shape),
+        keras.Input(shape=(28,28,1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
