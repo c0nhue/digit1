4cb3cc47417cfd52f7aff5fe72c543203ff89c39
c0nhue <maxsnp8@gmail.com>
2025-09-02 16:21:37 +0400

    added new model

--- src/model.py
+++ src/model.py
@@ -1,9 +1,9 @@
 from tensorflow import keras
 from tensorflow.keras import layers, models
 
-def build_model(num_classes=10):
-    """Define CNN model for MNIST classification."""
-    model = models.Sequential([
+def build_model_A(num_classes=10):
+    """Define deeper CNN model for MNIST classification."""
+    model_A = models.Sequential([
         keras.Input(shape=(28,28,1)),
         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
@@ -13,7 +13,23 @@ def build_model(num_classes=10):
         layers.Dense(128, activation="relu"),
         layers.Dense(num_classes, activation="softmax")
     ])
-    model.compile(optimizer="adam",
+    model_A.compile(optimizer="adam",
                   loss="categorical_crossentropy",
                   metrics=["accuracy"])
-    return model
+    return model_A
+
+
+def build_model_B(num_classes=10):
+    """Define simpler CNN model for MNIST classification."""
+    model_B = models.Sequential([
+        keras.Input(shape=(28,28,1)),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(num_classes, activation="softmax")
+    ])
+    model_B.compile(optimizer="adam",
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
+    return model_B
